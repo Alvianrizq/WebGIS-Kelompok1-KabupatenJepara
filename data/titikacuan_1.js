fde3b47795319f76c4caf0a6a65c35bbcf4d74d6
Alvianrizq <75605714+Alvianrizq@users.noreply.github.com>
2020-12-22 02:44:13 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,1 @@
+var json_titikacuan_1 = {"type":"FeatureCollection","name":"titikacuan_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.75160970147806,-6.611116878801946]}}]}
